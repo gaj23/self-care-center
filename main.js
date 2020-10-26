6cce75515133a684a066b9149b1c65a75e34af0d
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2020-10-25 22:31:35 -0400

    Edit generateMessage function so that the affirmations and mantras arrays are all used before an alert appears

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-var affirmation = ["I forgive myself and set myself free.", "I believe I can be all that I want to be.", "I am in the process of becoming the best version of myself.", "I have the freedom & power to create the life I desire.", "I choose to be kind to myself and love myself unconditionally.", "My possibilities are endless.", "I am worthy of my dreams.", "I am enough.", "I deserve to be healthy and feel good.", "I am full of energy and vitality and my mind is calm and peaceful.", "Every day I am getting healthier and stronger.", "I honor my body by trusting the signals that it sends me.", "I manifest perfect health by making smart choices."];
+var affirmations = ["I forgive myself and set myself free.", "I believe I can be all that I want to be.", "I am in the process of becoming the best version of myself.", "I have the freedom & power to create the life I desire.", "I choose to be kind to myself and love myself unconditionally.", "My possibilities are endless.", "I am worthy of my dreams.", "I am enough.", "I deserve to be healthy and feel good.", "I am full of energy and vitality and my mind is calm and peaceful.", "Every day I am getting healthier and stronger.", "I honor my body by trusting the signals that it sends me.", "I manifest perfect health by making smart choices."];
 
-var mantra = ["Breathing in, I send myself love. Breathing out, I send love to someone else who needs it.", "Don’t let yesterday take up too much of today.", "Every day is a second chance.", "Tell the truth and love everyone.", "I am free from sadness.", "I am enough.", "In the beginning it is you, in the middle it is you and in the end it is you.", "I love myself.", "I am present now.", "Inhale the future, exhale the past.", "This too shall pass.", "Yesterday is not today.", "The only constant is change.", "Onward and upward.", "I am the sky, the rest is weather."];
-console.log(mantra.length);
-console.log(affirmation.length);
-
+var mantras = ["Breathing in, I send myself love. Breathing out, I send love to someone else who needs it.", "Don’t let yesterday take up too much of today.", "Every day is a second chance.", "Tell the truth and love everyone.", "I am free from sadness.", "I am enough.", "In the beginning it is you, in the middle it is you and in the end it is you.", "I love myself.", "I am present now.", "Inhale the future, exhale the past.", "This too shall pass.", "Yesterday is not today.", "The only constant is change.", "Onward and upward.", "I am the sky, the rest is weather."];
+console.log(mantras.length);
+console.log(affirmations.length);
 
 var currentMessage = "";
-var messageArray = [];
 
 var radioSelection = document.querySelectorAll('input[type="radio"]');
 var messageButton = document.querySelector('.messageButton');
@@ -23,28 +21,36 @@ messageButton.addEventListener('click', generateMessage);
 //how can I account for having someone just clicking the button?
 
 function generateMessage() {
+  var usedAffirmation = getRandomIndex(affirmations);
+  var usedMantra = getRandomIndex(mantras);
 
   if (radioSelection[0].checked) {
-    currentMessage = affirmation[getRandomIndex(affirmation)];
+    currentMessage = affirmations[usedAffirmation];
     manageMessagesDisplayed(currentMessage);
+    affirmations.splice(usedAffirmation, 1);
+    console.log('this is the affirmations array', affirmations, 'this is the current message',
+      currentMessage);
   } else {
-    currentMessage = mantra[getRandomIndex(mantra)];
+    currentMessage = mantras[usedMantra];
     manageMessagesDisplayed(currentMessage);
+    mantras.splice(currentMessage);
+    console.log('this is the mantras array', mantras, 'this is the current message',
+      currentMessage);
   }
+
   message.innerText = currentMessage;
   revealMessage();
-  console.log(messageArray);
 }
 //how can I make this more dynamic with a for loop? (radio buttons are arrays?!; another document.selector?)
 
 function manageMessagesDisplayed() {
 
-  if (!messageArray.includes(currentMessage)) {
-    return messageArray.push(currentMessage);
-  } else {
-    alert("I'm sorry, you've reached the end of our avaliable uplifting messages. Press 'ok' if you'd like to go through them again!");
-    messageArray = [];
+  if (!affirmations.length) {
+    alert("I'm sorry, you've reached the end of our avaliable uplifting messages. Press 'ok' if you'd like to go through them again!")
+    console.log(affirmations);
   }
+  //how can I repopulate an empty array on the dom??
+  //how to prevent undefined from appearing?
 }
 
 function getRandomIndex(array) {
